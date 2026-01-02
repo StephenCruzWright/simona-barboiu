05521993127abd662ec440ceb42ba2d9ecaa1d84
stephencruzwright <stephencruzwright@gmail.com>
2026-01-02 23:01:05 +0000

    remove caps (2d/3d)

--- app/2D/illustration/mirage/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function MiragePage() {
-  return (
-    <main className="min-h-screen">
-      <h1>Mirage</h1>
-      <p>...</p>
-    </main>
-  );
-}
--- app/3D/games/paxvr/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function PaxVRPage() {
-  return (
-    <main className="min-h-screen">
-      <h1>PaxVR</h1>
-      <p>...</p>
-    </main>
-  );
-}
