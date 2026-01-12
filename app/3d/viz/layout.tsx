4f72ef9018a589f428e291cde037238a19aa81b8
stephencruzwright <stephencruzwright@gmail.com>
2026-01-12 11:35:08 +0000

    sidetitle wrap on mobile

--- app/3d/viz/layout.tsx
+++ app/3d/viz/layout.tsx
@@ -3,7 +3,7 @@ import SideTitle from "@/components/SideTitle";
 export default function VizLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid grid-cols-1 gap-4">
-      <aside className="sm:flex justify-end pt-4">
+      <aside className="justify-end pt-4 whitespace-nowrap">
         <SideTitle />
       </aside>
       <main>{children}</main>
