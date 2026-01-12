27b16a61a3b0279d9c718eb2c9958c9c035583b3
stephencruzwright <stephencruzwright@gmail.com>
2026-01-12 11:33:21 +0000

    side title not hidden

--- app/3d/viz/layout.tsx
+++ app/3d/viz/layout.tsx
@@ -3,7 +3,7 @@ import SideTitle from "@/components/SideTitle";
 export default function VizLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid grid-cols-1 gap-4">
-      <aside className="sm:flex justify-end pt-4 hidden">
+      <aside className="sm:flex justify-end pt-4">
         <SideTitle />
       </aside>
       <main>{children}</main>
